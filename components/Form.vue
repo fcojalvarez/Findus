53f8ae184aadc247886246a972137566e73fc97b
Francisco J. Álvarez <fcojalvarezrodriguez@gmail.com>
2020-06-01 19:03:27 +0200

    up form

---
+++ components/Form.vue
@@ -0,0 +1,183 @@
+<template>
+    <div class="container">
+    <div>
+      <form v-show="!seleccion.form1">
+        <h3 class="subtitle">¿Qué sistema operativo deseas?</h3>
+        <el-radio-group v-model="infoData.sistemaOperativo">
+          <el-radio-button label="Android"></el-radio-button>
+          <el-radio-button label="IOS"></el-radio-button>
+          <el-radio-button label="Cualquiera"></el-radio-button>
+        </el-radio-group>
+        <br>
+        <el-button class="btnPrim" type="primary" @click.prevent="useOk">Siguiente</el-button>
+      </form>
+      <form v-if="!seleccion.form2">
+        <h3 class="subtitle">¿Tamaño de la pantalla?</h3>
+        <el-radio-group v-model="infoData.pantalla">
+          <el-radio-button label="Menos de 5'"></el-radio-button>
+          <el-radio-button label="Entre 5' y 6'"></el-radio-button>
+          <el-radio-button label="Más de 6'"></el-radio-button>
+        </el-radio-group>
+        <br>
+        <el-button class="btnPrim" type="primary" @click.prevent="pantallaOk">Siguiente</el-button>
+      </form>
+      <form v-if="!seleccion.form3">
+        <h3 class="subtitle">¿Cuánto le importan las cámaras?</h3>
+        <p class="subtitle textDesc">Cámara frontal</p>
+        <el-radio-group v-model="infoData.camaraFrontal">
+          <el-radio-button label="Poco"></el-radio-button>
+          <el-radio-button label="Normal"></el-radio-button>
+          <el-radio-button label="Mucho"></el-radio-button>
+        </el-radio-group>
+        <br>
+        <p class="subtitle textDesc">Cámara trasera</p>
+        <el-radio-group v-model="infoData.camaraTrasera">
+          <el-radio-button label="Poco"></el-radio-button>
+          <el-radio-button label="Normal"></el-radio-button>
+          <el-radio-button label="Mucho"></el-radio-button>
+        </el-radio-group>
+        <br>
+        <el-button class="btnPrim" type="primary" @click.prevent="camarasOk">Siguiente</el-button>
+      </form>
+      <form v-if="!seleccion.form4">
+        <h3 class="subtitle">¿Cuánta memoria RAM?</h3>
+        <el-radio-group v-model="infoData.memoriaRam">
+          <el-radio-button label="Menos de 8GB"></el-radio-button>
+          <el-radio-button label="Más de 8GB"></el-radio-button>
+        </el-radio-group>
+        <br>
+        <el-button class="btnPrim" type="primary" @click.prevent="ramOk">Siguiente</el-button>
+      </form>
+      <form v-if="!seleccion.form5">
+        <h3 class="subtitle">¿Cuánto almacenamiento interno?</h3>
+        <el-radio-group v-model="infoData.almacenamiento">
+          <el-radio-button label="64GB"></el-radio-button>
+          <el-radio-button label="128GB"></el-radio-button>
+          <el-radio-button label="Más de 128GB"></el-radio-button>
+        </el-radio-group>
+        <br>
+        <el-button class="btnPrim" type="primary" @click.prevent="almacenamientoOk">Siguiente</el-button>
+      </form>
+      <form v-if="!seleccion.form6">
+        <h3 class="subtitle">¿Rango de precio?</h3>
+        <div class="block">
+            <span class="textDesc">{{infoData.precio[0]}}€ - {{infoData.precio[1]}}€</span>
+            <el-slider
+            v-model="infoData.precio"
+            range
+            show-stops
+            :min="100"
+            :max="1400">
+            </el-slider>
+        </div>
+        <br>
+        <el-button class="btnPrim" type="primary" @click.prevent="precioOk">Siguiente</el-button>
+      </form>
+      <form v-if="!seleccion.form7">
+        <h3 class="subtitle">¿Le interesa alguna característica más?</h3>
+        <div>
+            <el-checkbox-group v-model="infoData.more">
+            <el-checkbox-button v-for="item in features" :label="item" :key="item">{{item}}</el-checkbox-button>
+            </el-checkbox-group>
+        </div>
+        <br>
+        <el-button class="btnPrim" type="primary" @click.prevent="moreOk">Siguiente</el-button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+const featuresOptions = ['Sensor de huella', 'Desbloqueo facial', 'Carga inalámbrica', 'Carga rápida', 'Radio FM', 'Dual SIM', 'Jack 3.5mm', 'Actualizaciones']
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      seleccion: {
+        form1: '',
+        form2: 'false',
+        form3: 'false',
+        form4: 'false',
+        form5: 'false',
+        form6: 'false',
+        form7: 'false'
+      },
+      infoData: {
+        sistemaOperativo: '',
+        pantalla: '',
+        camaraFrontal: '',
+        camaraTrasera: '',
+        almacenamiento: '',
+        precio: [100,1000],
+        more: [],
+      },
+      features: featuresOptions
+    };
+  },
+  methods: {
+    useOk(){
+      this.seleccion.form1 = true;
+      this.seleccion.form2 = false;
+    },
+    pantallaOk(){
+      this.seleccion.form2 = true;
+      this.seleccion.form3 = false;
+    },
+    camarasOk(){
+      this.seleccion.form3 = true;
+      this.seleccion.form4 = false;
+    },
+    ramOk(){
+      this.seleccion.form4 = true;
+      this.seleccion.form5 = false;
+    },
+    almacenamientoOk(){
+      this.seleccion.form5 = true;
+      this.seleccion.form6 = false;
+    },
+    precioOk(){
+      this.seleccion.form6 = true;
+      this.seleccion.form7 = false;
+    },
+    moreOk(){
+      this.seleccion.form7 = true;
+      console.log(this.infoData)
+    }
+  }
+}
+</script>
+
+<style>
+
+.container {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.btnPrim{
+  margin: 20px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 30px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.textDesc{
+  font-size: 15px;
+    margin-top: 10px;
+    margin-bottom:5px;
+    color: #444;
+    padding-bottom: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Form></Form>
+</template>
+
+<script>
+import Form from '@/components/Form'
+
+export default {
+  components: {
+    Form
+  },
+  data () {
+    return {
+  }
+}
+}
+</script>
